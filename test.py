df889f591ef39c8618d93dd8d8a477c38d245a9e
Cadence1988 <ryan.marlow@sdgku.edu>
2024-09-06 16:24:04 -1000

    first commit

---
+++ test.py
@@ -0,0 +1,43 @@
+print('hello world')
+# console.log('hello from python);
+
+# let variable = 1;
+variable = 1
+variable = True
+variable = 'hello'
+
+array = [1, 2, 3, 4, 5] 
+print(array[4])
+array.append(6)
+print(array)
+array.pop()
+print(array)
+
+dict = {
+    'name':'Ryan',
+    'last_name':'Marlow'
+    }
+
+print(dict)
+print(dict['name'])
+
+# for(i=0; i<size; i++)
+# let temp = array[i]
+#console.log
+
+for element in dict:
+    print ('Ryan')
+
+for i in dict:
+    print(dict[i])
+
+def hello():
+    print('hello from python function')
+
+hello()
+
+input('pick a number 1 or 2: ')
+
+
+
+
